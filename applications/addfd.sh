5ba41d0dee6e45d82011bd03b1282a8cf9ea5a22
rezapace <m.rezahidayat.rh@gmail.com>
2024-08-19 17:13:08 +0700

    update

--- applications/addfd.sh
+++ applications/addfd.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Meminta input nama aplikasi dan perintah eksekusi dari pengguna
+# Meminta input nama aplikasi dan path dari pengguna
 read -p "Masukkan nama aplikasi: " app_name
-read -p "Masukkan perintah eksekusi (Exec): " app_exec
+read -p "Masukkan path aplikasi: " app_path
+
+# Menambahkan 'caja' di depan path
+app_exec="caja $app_path"
 
 # Membuat konten untuk file .desktop
-desktop_content="[Desktop Entry]\nVersion=1.0\nType=Application\nName=$app_name\nExec=$app_exec\nIcon=folder\nTerminal=false"
+desktop_content="[Desktop Entry]\nVersion=1.0\nType=Application\nName=$app_name\nExec=$app_exec\nIcon=folder\nTerminal=false\n"
 
 # Menyimpan konten ke file .desktop
+mkdir -p "/home/r/.local/share/applications/folder"
 echo -e "$desktop_content" > "/home/r/.local/share/applications/folder/$app_name.desktop"
 
 echo "File $app_name.desktop telah berhasil dibuat di /home/r/.local/share/applications/folder."
+
